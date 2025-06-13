b029866b01f34d182d629a0fac8fe332514af0ac
Kamil Erguler <kerguler@gmail.com>
2025-06-13 10:44:04 +0300

    Update restart-docker.sh

--- restart-docker.sh
+++ restart-docker.sh
@@ -3,11 +3,11 @@
 source veclim-data-server/.env
 
 docker build --build-arg VEC_PORTE=${VEC_PORTE} \
-             -t veclim-data-server-test \
+             -t veclim-data-server \
              .
 docker run -d \
            -p ${VEC_PORTE}:${VEC_PORT} \
            --restart always \
-           --name veclim-data-server-test \
+           --name veclim-data-server \
            -v ${VEC_DATA}:${DIR_DATA} \
-           veclim-data-server-test
+           veclim-data-server

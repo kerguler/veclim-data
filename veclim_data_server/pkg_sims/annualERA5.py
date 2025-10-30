30c11d6564e69a290ed42f2f037c3dfb44a3a9f7
Kamil Erguler <kerguler@gmail.com>
2025-10-30 18:12:09 +0200

    cache

--- veclim_data_server/pkg_sims/annualERA5.py
+++ veclim_data_server/pkg_sims/annualERA5.py
@@ -1,7 +1,7 @@
 import numpy
 
 from ..environ import DIR_DATA
-from ..functions import xr_open_lazy, daylength
+from ..functions import xr_open_lazy, daylength, cache_npy
 
 print("Loading annual ERA5...",flush=True)
 
@@ -10,10 +10,14 @@ dr = "%s/clim/ERA5/ERA5_single_levels_decadal/2010_to_2019" %(DIR_DATA)
 atemp = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_2m_temperature.nc" %(dr))
 tf = 365
 latitude = atemp['lat']
-photo = numpy.array([[daylength(lat, d) for d in numpy.arange(tf)] for lat in latitude])
 atemp = atemp['2m_temperature']
 atmin = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_2m_temperature_min.nc" %(dr))['2m_temperature_min']
 atmax = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_2m_temperature_max.nc" %(dr))['2m_temperature_max']
 rehum = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_2m_relative_humidity.nc" %(dr))['2m_relative_humidity']
 precp = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_total_precipitation.nc" %(dr))['total_precipitation']
 soilw = xr_open_lazy("%s/ERA5_ERA5_single_levels_decadal_2010_to_2019_volumetric_soil_water_layer_1.nc" %(dr))['volumetric_soil_water_layer_1']
+
+def calc_dat():
+    return numpy.array([[daylength(lat, d) for d in numpy.arange(tf)] for lat in latitude])
+
+photo = cache_npy("%s/photoperiod_decadal_2010_to_2019.npy" %(dr),calc_dat)

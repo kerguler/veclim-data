40d62dad9bde417e64b50ea3fb65aea923cbb94d
Kamil Erguler <kerguler@gmail.com>
2025-11-04 13:48:26 +0200

    fixes

--- veclim_data_server/pkg_sims/annualVectorA.py
+++ veclim_data_server/pkg_sims/annualVectorA.py
@@ -3,19 +3,17 @@ import numpy
 from ..environ import DIR_DATA
 from ..functions import xr_open_lazy
 
-print("Loading VectorA...",flush=True)
-
 decade = "2010-2019"
 dlabel = "2010_to_2019"
-dr = "%s/sims/vector08c_Q4.a100+1/ERA5_single_levels_decadal/%s" %(DIR_DATA,dlabel)
-colegg = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_colegg.nc" %(dr,dlabel))['colegg']
-colK = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_colK.nc" %(dr,dlabel))['colK']
-coln2 = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_coln2.nc" %(dr,dlabel))['coln2']
-coln4f = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_coln4f.nc" %(dr,dlabel))['coln4f']
+dr_vec = "%s/sims/vector08c_Q4.a100+1/ERA5_single_levels_decadal/%s" %(DIR_DATA,dlabel)
+colegg = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_colegg.nc" %(dr_vec,dlabel))['colegg']
+colK = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_colK.nc" %(dr_vec,dlabel))['colK']
+coln2 = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_coln2.nc" %(dr_vec,dlabel))['coln2']
+coln4f = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_ERA5_single_levels_decadal_%s_coln4f.nc" %(dr_vec,dlabel))['coln4f']
 
-dr = "%s/sims/vector08c_Q4.a100+1_chikv_QI/ERA5_single_levels_decadal/p4000r100w60/%s" %(DIR_DATA,dlabel)
-iouts = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_chikv_QI_ERA5_single_levels_decadal_p4000r100w60_%s_iouts.nc" %(dr,dlabel))['iouts']
-pouts = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_chikv_QI_ERA5_single_levels_decadal_p4000r100w60_%s_pouts.nc" %(dr,dlabel))['pouts']
+dr_dis = "%s/sims/vector08c_Q4.a100+1_chikv_QI/ERA5_single_levels_decadal/p4000r100w60/%s" %(DIR_DATA,dlabel)
+iouts = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_chikv_QI_ERA5_single_levels_decadal_p4000r100w60_%s_iouts.nc" %(dr_dis,dlabel))['iouts']
+pouts = xr_open_lazy("%s/sims_vector08c_Q4.a100+1_chikv_QI_ERA5_single_levels_decadal_p4000r100w60_%s_pouts.nc" %(dr_dis,dlabel))['pouts']
 
 tmp = numpy.genfromtxt("%s/surveillance/Italy2008/coord_albopictus_Italy2008.csv" %(DIR_DATA), delimiter=',',names=True)
 acc = {}

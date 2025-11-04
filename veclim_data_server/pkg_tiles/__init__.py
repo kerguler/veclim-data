b403ec2ddbc6b168b677c19cb2f5768b9aa916d7
Kamil Erguler <kerguler@gmail.com>
2025-11-04 12:17:08 +0200

    Update __init__.py

--- veclim_data_server/pkg_tiles/__init__.py
+++ veclim_data_server/pkg_tiles/__init__.py
@@ -6,10 +6,9 @@ for _, name, _ in pkgutil.iter_modules(__path__):
 
 tile_dat = {}
 for tile in modules:
-    for dat in modules[tile].tile_dat:
-        if dat in tile_dat:
-            print("WARNING: Replacing %s with version %s!" %(dat,tile))
-        tile_dat[dat] = modules[tile].tile_dat[dat]
+    if tile in tile_dat:
+        print("WARNING: Replacing %s!" %(tile))
+    tile_dat[tile] = modules[tile].tile_dat
 
 print("tile_dat:",flush=True)
 print(tile_dat,flush=True)

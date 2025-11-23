28bffff23bc45f1eedcc2833bcc56c6efc4eb624
Kamil Erguler <kerguler@gmail.com>
2025-11-23 10:44:17 +0200

    Update papatasi_V2511A.py

--- veclim_data_server/pkg_models/papatasi_V2511A.py
+++ veclim_data_server/pkg_models/papatasi_V2511A.py
@@ -2,7 +2,7 @@ import json
 import numpy
 
 from ..response import empty_response, returnResponse
-from ..functions import get_dates, get_clim, getIndex, remove_feb29
+from ..functions import get_dates, get_clim, isel, remove_feb29
 import veclim_data_server.pkg_sims as pkg_sims
 
 def get_location(lon, lat):
@@ -24,9 +24,9 @@ def get_papatasi_days(loni, lati, idates, isFeb29):
     papatasi = pkg_sims.modules['papatasi_V2511A']
     #
     return {
-        "simL": remove_feb29(papatasi.female_lo.isel(x=loni,y=lati),idates,isFeb29),
-        "simH": remove_feb29(papatasi.female_hi.isel(x=loni,y=lati),idates,isFeb29),
-        "simM": remove_feb29(papatasi.female_mn.isel(x=loni,y=lati),idates,isFeb29)
+        "simL": remove_feb29(isel(papatasi.female_lo,x=loni,y=lati),idates,isFeb29),
+        "simH": remove_feb29(isel(papatasi.female_hi,x=loni,y=lati),idates,isFeb29),
+        "simM": remove_feb29(isel(papatasi.female_mn,x=loni,y=lati),idates,isFeb29)
     }
 
 def get_sandfly(lon, lat, date0, date1=False, ts=False):

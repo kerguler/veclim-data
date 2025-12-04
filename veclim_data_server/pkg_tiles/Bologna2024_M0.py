d3148d6359cb25dc8570b349306112ce032d71fa
Kamil Erguler <kerguler@gmail.com>
2025-12-04 21:33:31 +0200

    Update Bologna2024_M0.py

--- veclim_data_server/pkg_tiles/Bologna2024_M0.py
+++ veclim_data_server/pkg_tiles/Bologna2024_M0.py
@@ -8,7 +8,7 @@ from ..environ import DIR_DATA
 from ..fun_tiles import getTiles
 
 model = 0
-mlbl = "albopictus\ (sPop)"
+mlbl = 'albopictus (sPop)'
 
 clscl = ['#00000000', '#fbe590', '#fcc65a', '#f7a034', '#f47b2c', '#e85229', '#d82929', '#931b1f']
 clbins = [-4,-3,-2,-1,0,1,2,3,4]
@@ -17,7 +17,7 @@ cllbl = ["1/16 (%s)" %(mlbl),"1/8","1/4","1/2","1","2","4","8","16"]
 cmap = mpl.colors.ListedColormap([mpl.colors.to_rgba(c) for c in clscl])
 norm = mpl.colors.BoundaryNorm(clbins, cmap.N, clip=True, extend='neither')
 
-dat = numpy.load("%s/sims/Bologna2024/tile_dat_%d_%s.npy" %(DIR_DATA,model,mlbl),mmap_mode='r')
+dat = numpy.array([]) # numpy.load("%s/sims/Bologna2024/tile_dat_%d_%s.npy" %(DIR_DATA,model,mlbl),mmap_mode='r')
 
 tile_dat = {
     'label': label,

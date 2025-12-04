4ccf4f29fd63acf381968b1a8efc9913d16d73cf
Kamil Erguler <kerguler@gmail.com>
2025-12-04 23:31:18 +0200

    Update Bologna2024_vabun.py

--- veclim_data_server/pkg_tiles/Bologna2024_vabun.py
+++ veclim_data_server/pkg_tiles/Bologna2024_vabun.py
@@ -15,7 +15,7 @@ cmap = mpl.colors.ListedColormap([mpl.colors.to_rgba(c) for c in clscl])
 norm = mpl.colors.BoundaryNorm(clbins, cmap.N, clip=True, extend='neither')
 
 def calc_dat():
-    x = Bologna2024.surv.eggs.load().values > 0.0
+    x = Bologna2024.surv.eggs.mean(dim='yrwk').load().values > 0.0
     return numpy.array(x,dtype=numpy.float64)
 
 dat = cache_npy("tile_dat_Bologna2024.npy", calc_dat)

952240e2c9af8896ba2b5b4b5e2442837950e2c5
Kamil Erguler <kerguler@gmail.com>
2025-12-04 07:10:22 +0200

    Update Bologna2024.py

--- veclim_data_server/pkg_surv/Bologna2024.py
+++ veclim_data_server/pkg_surv/Bologna2024.py
@@ -3,4 +3,4 @@ from ..functions import xr_open_lazy
 
 print("Loading surv_Bologna2024...",flush=True)
 
-surv = xr_open_lazy("%s/Bologna2024/surv_Bologna2024.nc" %(DIR_DATA))
+surv = xr_open_lazy("%s/surveillance/Bologna2024/surv_Bologna2024.nc" %(DIR_DATA))

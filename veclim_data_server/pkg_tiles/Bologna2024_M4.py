7eb60dd21c1445edb5f52adc73dc49b786a1fe7f
Kamil Erguler <kerguler@gmail.com>
2025-12-05 23:40:00 +0200

    colour key update

--- veclim_data_server/pkg_tiles/Bologna2024_M4.py
+++ veclim_data_server/pkg_tiles/Bologna2024_M4.py
@@ -12,7 +12,7 @@ mlbl = 'VECTRI'
 
 clscl = ['#00000000', '#fbe590', '#fcc65a', '#f7a034', '#f47b2c', '#e85229', '#d82929', '#931b1f']
 clbins = [-4,-3,-2,-1,0,1,2,3,4]
-cllbl = ["1/16 (%s)" %(mlbl),"1/8","1/4","1/2","1","2","4","8","16"]
+cllbl = ["1/16","1/8","1/4","1/2","1","2","4","8","16"]
 
 cmap = mpl.colors.ListedColormap([mpl.colors.to_rgba(c) for c in clscl])
 norm = mpl.colors.BoundaryNorm(clbins, cmap.N, clip=True, extend='neither')

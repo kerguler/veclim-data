63fee2f57fe1c0d436df8b1c00d9436a438ed77e
Kamil Erguler <kerguler@gmail.com>
2025-12-10 12:04:12 +0200

    Update Bologna2024_vabun.py

--- veclim_data_server/pkg_tiles/Bologna2024_vabun.py
+++ veclim_data_server/pkg_tiles/Bologna2024_vabun.py
@@ -8,15 +8,25 @@ from ..functions import cache_npy
 from ..fun_tiles import getBologna2024Tiles
 from ..pkg_surv import Bologna2024
 
-clscl = ['#00000000', '#f15a48']
-clbins = [0,1]
-cllbl = ["Unknown/absent", "VectAbundance (2010-2022)"]
+clscl = ['#00000000', '#fbe590', '#f7a034', '#e85229', '#931b1f']
+clbins = [0,1,2,3,4,5]
+cllbl = ["Unknown/absent", "VectAbundance (2019-2022) - 1 year", "VectAbundance (2019-2022) - 2 years", "VectAbundance (2019-2022) - 3 years", "VectAbundance (2019-2022) - 4 years"]
+
 cmap = mpl.colors.ListedColormap([mpl.colors.to_rgba(c) for c in clscl])
 norm = mpl.colors.BoundaryNorm(clbins, cmap.N, clip=True, extend='neither')
 
 def calc_dat():
-    x = Bologna2024.surv.eggs.mean(dim='yrwk').load().values > 0.0
-    return numpy.array(x,dtype=numpy.float64)
+    mat = Bologna2024.surv
+    years = numpy.array([s.split('-')[0] for s in mat.yrwk.values])
+    unique_years = numpy.unique(years)
+    #
+    counts = numpy.zeros((mat.sizes['lat'], mat.sizes['lon']), dtype=int)
+    for y in unique_years:
+        mask = years == y
+        any_non_nan = mat.eggs.isel(yrwk=mask).notnull().any(dim='yrwk')
+        counts += any_non_nan.values.astype(int)
+    #
+    return counts
 
 dat = cache_npy("tile_dat_Bologna2024.npy", calc_dat)
 
